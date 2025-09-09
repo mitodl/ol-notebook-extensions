e21d10f715686eae24ad67349a92c51d100a3454
dansubak <dansubak@mit.edu>
2025-09-09 15:29:12 -0400

    Frontend extension to add MIT logo to notebook view in Jupyterhub-hosted notebooks

---
+++ ol-themed-jupyter/src/index.ts
@@ -0,0 +1,62 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { LabIcon } from '@jupyterlab/ui-components';
+import { Widget } from '@lumino/widgets';
+import { PageConfig } from '@jupyterlab/coreutils';
+// @ts-expect-error: Suppressing extraneous error
+import mitJupyterSvgStr from '../style/logo.svg';
+
+export const mitJupyterIcon = new LabIcon({
+  name: 'ui-components:mit-jupyter',
+  svgstr: mitJupyterSvgStr
+});
+/**
+ * Initialization data for the ol-themed-jupyter extension.
+ */
+/*const plugin = {
+  id: 'ol-themed-jupyter:plugin',
+  description: 'Provides MIT OpenLearning themes for Jupyter',
+  autoStart: true,
+  requires: [ILabShell],
+  activate: (app: JupyterFrontEnd, shell: ILabShell) => {
+    const logo = new Widget();
+    mitJupyterIcon.element({
+      container: logo.node,
+      elementPosition: 'center',
+      margin: '2px 2px 2px 2px',
+      height: 'auto',
+      width: '32px'
+    });
+    logo.id = 'jp-MainLogo';
+    shell.add(logo, 'top', { rank: 0 });
+  }
+};*/
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'ol-themed-jupyter:plugin',
+  description: 'Provides MIT OpenLearning themes for Jupyter',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    const baseUrl = PageConfig.getBaseUrl();
+    const node = document.createElement('a');
+    node.href = `${baseUrl}tree`;
+    node.target = '_blank';
+    node.rel = 'noopener noreferrer';
+    const logo = new Widget({ node });
+
+    mitJupyterIcon.element({
+      container: node,
+      elementPosition: 'center',
+      padding: '2px 2px 2px 8px',
+      height: '28px',
+      width: 'auto',
+      cursor: 'pointer',
+      margin: 'auto'
+    });
+    logo.id = 'jp-NotebookLogo';
+    app.shell.add(logo, 'top', { rank: 0 });
+  }
+};
+
+export default plugin;
